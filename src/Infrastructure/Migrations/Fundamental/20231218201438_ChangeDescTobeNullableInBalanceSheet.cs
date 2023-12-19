8cab33316acbd4f70825b758b2f9caaae9f195c2
Mostafa Esmaeili <mostafa.esmaeili8@gmail.com>
2023-12-19 07:15:07 +0330

    Migrations

---
+++ src/Infrastructure/Migrations/Fundamental/20231218201438_ChangeDescTobeNullableInBalanceSheet.cs
@@ -0,0 +1,42 @@
+﻿#nullable disable
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fundamental.Migrations.Fundamental
+{
+    /// <inheritdoc />
+    public partial class ChangeDescTobeNullableInBalanceSheet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "fs",
+                table: "BalanceSheet",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "fs",
+                table: "BalanceSheet",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+        }
+    }
+}
