8bbdcb153fd1cb4e754afa9cc5074c8ead4d0c64
Mostafa Esmaeili <mostafa.esmaeili8@gmail.com>
2023-12-28 20:23:13 +0330

    Income statement details

---
+++ src/Application/Fundamental.Application/Codals/Manufacturing/Queries/GetIncomeStatementDetails/GetIncomeStatementDetailsErrorCode.cs
@@ -0,0 +1,7 @@
+﻿using Fundamental.ErrorHandling.Attributes;
+using Fundamental.ErrorHandling.Enums;
+
+namespace Fundamental.Application.Codals.Manufacturing.Queries.GetIncomeStatementDetails;
+
+[HandlerCode(HandlerCode.GetIncomeStatementDetails)]
+public enum GetIncomeStatementDetailsErrorCode;
---
+++ src/Application/Fundamental.Application/Codals/Manufacturing/Queries/GetIncomeStatementDetails/GetIncomeStatementDetailsQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using Fundamental.Application.Codals.Manufacturing.Specifications;
+using Fundamental.Domain.Codals.Manufacturing.Entities;
+using Fundamental.Domain.Repositories.Base;
+using Fundamental.ErrorHandling;
+using MediatR;
+
+namespace Fundamental.Application.Codals.Manufacturing.Queries.GetIncomeStatementDetails;
+
+public sealed class GetIncomeStatementDetailsQueryHandler(IRepository<IncomeStatement> repository)
+    : IRequestHandler<GetIncomeStatementDetailsRequest, Response<List<GetIncomeStatementDetailsResultDto>>>
+{
+    public async Task<Response<List<GetIncomeStatementDetailsResultDto>>> Handle(
+        GetIncomeStatementDetailsRequest request,
+        CancellationToken cancellationToken
+    )
+    {
+        List<GetIncomeStatementDetailsResultDto> data = await repository.ListAsync(
+            IncomeStatementDetailSpec.Where(request.TraceNo, request.FiscalYear, request.ReportMonth),
+            cancellationToken);
+
+        return data;
+    }
+}
