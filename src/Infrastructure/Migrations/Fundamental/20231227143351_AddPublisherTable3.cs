c44486240fb64769effd9aca502e8efed13a16e2
Mostafa Esmaeili <mostafa.esmaeili8@gmail.com>
2023-12-28 12:29:06 +0330

    Update Migration

---
+++ src/Infrastructure/Migrations/Fundamental/20231227143351_AddPublisherTable3.cs
@@ -0,0 +1,45 @@
+﻿#nullable disable
+
+using Fundamental.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fundamental.Migrations.Fundamental
+{
+    [DbContext(typeof(FundamentalDbContext))]
+    [Migration("20231227143351_AddPublisherTable3")]
+    public class AddPublisherTable3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ActivitySubject",
+                schema: "fs",
+                table: "Publisher",
+                type: "nvarchar(2048)",
+                maxLength: 2048,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(512)",
+                oldMaxLength: 512,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ActivitySubject",
+                schema: "fs",
+                table: "Publisher",
+                type: "nvarchar(512)",
+                maxLength: 512,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2048)",
+                oldMaxLength: 2048,
+                oldNullable: true);
+        }
+    }
+}
