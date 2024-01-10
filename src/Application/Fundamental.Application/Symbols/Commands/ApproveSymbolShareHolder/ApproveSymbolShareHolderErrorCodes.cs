8f079f5e6530510cdc9c9b8f1cac2fc2226a989e
Mostafa Esmaeili <mostafa.esmaeili8@gmail.com>
2024-01-10 08:44:09 +0330

    Fix Swagger Problem

--- src/Application/Fundamental.Application/Symbols/Commands/ApproveSymbolShareHolder/ApproveSymbolShareHolderErrorCodes.cs
+++ src/Application/Fundamental.Application/Symbols/Commands/ApproveSymbolShareHolder/ApproveSymbolShareHolderErrorCodes.cs
@@ -6,6 +6,9 @@ namespace Fundamental.Application.Symbols.Commands.ApproveSymbolShareHolder;
 [HandlerCode(HandlerCode.ApproveSymbolShareHolder)]
 public enum ApproveSymbolShareHolderErrorCodes
 {
+    [ErrorType(BackendErrorType.Security)]
     IdIsNotValid = 11_394_101,
+
+    [ErrorType(BackendErrorType.Security)]
     ShareHolderIsinIsNotValid = 11_394_102,
 }
