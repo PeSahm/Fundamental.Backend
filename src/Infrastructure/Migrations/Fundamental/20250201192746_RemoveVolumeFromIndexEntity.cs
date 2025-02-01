84075bc6f41e58cd9f035f6590ec8a274be47d6b
Mostafa <mostafa.esmaeili8@gmail.com>
2025-02-01 23:06:50 +0330

    Refactor indices table schema in shd schema
    This change is necessary to streamline the database schema for the `indices`
    table in the `shd` schema. By removing the `volume1` column and renaming the
    `volume` column to `value`, we aim to improve clarity and maintain consistency
    in the database structure. The `FundamentalDbContextModelSnapshot` file is
    updated accordingly to ensure the model accurately reflects these schema
    modifications.

---
+++ src/Infrastructure/Migrations/Fundamental/20250201192746_RemoveVolumeFromIndexEntity.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fundamental.Migrations.Fundamental
+{
+    /// <inheritdoc />
+    public partial class RemoveVolumeFromIndexEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "volume1",
+                schema: "shd",
+                table: "indices");
+
+            migrationBuilder.RenameColumn(
+                name: "volume",
+                schema: "shd",
+                table: "indices",
+                newName: "value");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "value",
+                schema: "shd",
+                table: "indices",
+                newName: "volume");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "volume1",
+                schema: "shd",
+                table: "indices",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
