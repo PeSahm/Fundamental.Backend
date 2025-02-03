5a4030caff45b1da3b5ef5b19f72431244638727
Mostafa <mostafa.esmaeili8@gmail.com>
2025-02-04 00:11:25 +0330

    Refactor ErrorLoggingMiddleware to use primary constructor
    Refactored the ErrorLoggingMiddleware class to use a primary constructor for
    better readability and maintainability. This change eliminates the need for
    private fields `_next`, `_logger`, and `_ipService`, reducing redundancy and
    simplifying the code. The `InvokeAsync` method now directly uses the
    constructor parameters, enhancing clarity and reducing potential errors
    related to field initialization.

--- src/Presentation/Fundamental.WebApi/Middlewares/ErrorLoggingMiddleware.cs
+++ src/Presentation/Fundamental.WebApi/Middlewares/ErrorLoggingMiddleware.cs
@@ -2,31 +2,20 @@
 
 namespace Fundamental.WebApi.Middlewares;
 
-public class ErrorLoggingMiddleware
+public class ErrorLoggingMiddleware(RequestDelegate next, ILogger<ErrorLoggingMiddleware> logger, IIpService ipService)
 {
-    private readonly RequestDelegate _next;
-    private readonly ILogger _logger;
-    private readonly IIpService _ipService;
-
-    public ErrorLoggingMiddleware(RequestDelegate next, ILogger<ErrorLoggingMiddleware> logger, IIpService ipService)
-    {
-        _next = next;
-        _ipService = ipService;
-        _logger = logger;
-    }
-
     public async Task InvokeAsync(HttpContext httpContext)
     {
-        await _next(httpContext);
+        await next(httpContext);
 
         switch (httpContext.Response.StatusCode)
         {
             case 401:
             case 403:
-                string? ipAddress = _ipService.GetRawRemoteIpAddress();
+                string? ipAddress = ipService.GetRawRemoteIpAddress();
                 string? authorizationHeader = httpContext.Request.Headers["Authorization"];
 
-                _logger.LogWarning(
+                logger.LogWarning(
                     "HTTP status code '{StatusCode}' returned for authorization header '{AuthorizationHeader}' sent from '{RemoteIpAddress}'",
                     httpContext.Response.StatusCode,
                     authorizationHeader,
