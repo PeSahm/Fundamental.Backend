898e49ff441872a9154260cebab8d46e24075008
Mostafa <mostafa.esmaeili8@gmail.com>
2025-02-10 00:21:46 +0330

    Add GetIndices feature with error handling and repository
    Introduced a comprehensive GetIndices feature to enhance the system's ability to
    handle index-related queries. This includes the addition of a new enum for error
    codes, a query handler to process requests, and a repository for database
    operations. The changes aim to improve error handling, encapsulate request
    parameters, and provide a robust API endpoint for indices. The refactoring of
    the Index class and the inclusion of new DTOs and result items ensure better
    data representation and calculation methods. Dependency injection was updated
    to register the new repository, ensuring seamless integration and maintainability.

---
+++ src/Application/Fundamental.Application/Symbols/Repositories/IIndicesRepository.cs
@@ -0,0 +1,8 @@
+﻿using Fundamental.Application.Symbols.Queries.GetIndices;
+
+namespace Fundamental.Application.Symbols.Repositories;
+
+public interface IIndicesRepository
+{
+    Task<GetIndicesResultDto> GetIndices(GetIndicesRequest request, CancellationToken cancellationToken);
+}
---
+++ src/Presentation/Fundamental.WebApi/Controllers/IndicesController.cs
@@ -0,0 +1,21 @@
+﻿using ErrorHandling.AspNetCore;
+using Fundamental.Application.Symbols.Queries.GetIndices;
+using Fundamental.ErrorHandling;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fundamental.WebApi.Controllers
+{
+    [ApiController]
+    [Route("indices")]
+    [ApiVersion("1.0")]
+    [TranslateResultToActionResult]
+    public class IndicesController(IMediator mediator) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<Response<GetIndicesResultDto>> GetIndices([FromQuery] GetIndicesRequest request)
+        {
+            return await mediator.Send(request);
+        }
+    }
+}
