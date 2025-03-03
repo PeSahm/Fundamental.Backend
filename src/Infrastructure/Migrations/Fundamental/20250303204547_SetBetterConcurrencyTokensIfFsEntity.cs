5e6d9be414fd792065e4f40d4f258a6c6c86bac0
Mostafa <mostafa.esmaeili8@gmail.com>
2025-03-04 00:16:50 +0330

    Refactor FinancialStatement for improved concurrency control
    Enhancing the `FinancialStatement` class with a `ConcurrencyToken` property
    allows for better management of concurrent data access, reducing the risk of
    data conflicts. The removal of the `Version` property simplifies the model
    and aligns with best practices for concurrency handling.
    
    Additionally, the introduction of new properties for `DpsRatioLastYear`
    and `DpsRatioTwoYearsAgo` provides more comprehensive financial data
    representation, which is essential for accurate reporting and analysis.
    The corresponding database schema changes ensure that these updates are
    reflected in the underlying data structure, maintaining data integrity
    and consistency.

---
+++ src/Infrastructure/Migrations/Fundamental/20250303204547_SetBetterConcurrencyTokensIfFsEntity.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using Fundamental.Domain.Codals.Manufacturing.Enums;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fundamental.Migrations.Fundamental
+{
+    /// <inheritdoc />
+    public partial class SetBetterConcurrencyTokensIfFsEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "last_close_price_date",
+                schema: "manufacturing",
+                table: "financial-statement");
+
+            migrationBuilder.DropColumn(
+                name: "version",
+                schema: "manufacturing",
+                table: "financial-statement");
+
+            migrationBuilder.AlterColumn<List<NoneOperationalIncomeTag>>(
+                name: "tags",
+                schema: "manufacturing",
+                table: "non-operation-income-expense",
+                type: "none_operational_income_tag[]",
+                nullable: false,
+                defaultValue: new List<NoneOperationalIncomeTag>(),
+                oldClrType: typeof(List<NoneOperationalIncomeTag>),
+                oldType: "none_operational_income_tag[]",
+                oldDefaultValue: new List<NoneOperationalIncomeTag>());
+
+            migrationBuilder.AddColumn<uint>(
+                name: "xmin",
+                schema: "manufacturing",
+                table: "financial-statement",
+                type: "xid",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: 0u);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "xmin",
+                schema: "manufacturing",
+                table: "financial-statement");
+
+            migrationBuilder.AlterColumn<List<NoneOperationalIncomeTag>>(
+                name: "tags",
+                schema: "manufacturing",
+                table: "non-operation-income-expense",
+                type: "none_operational_income_tag[]",
+                nullable: false,
+                defaultValue: new List<NoneOperationalIncomeTag>(),
+                oldClrType: typeof(List<NoneOperationalIncomeTag>),
+                oldType: "none_operational_income_tag[]",
+                oldDefaultValue: new List<NoneOperationalIncomeTag>());
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "last_close_price_date",
+                schema: "manufacturing",
+                table: "financial-statement",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "version",
+                schema: "manufacturing",
+                table: "financial-statement",
+                type: "bytea",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+    }
+}
