53630695ad226ff996df534d86572f5ec051d7b9
Mostafa <mostafa.esmaeili8@gmail.com>
2025-04-20 15:16:17 +0330

    Refactors financial statement title generation
    Extracts duplicate title generation logic from balance sheet and income statement DTOs into a common utility class to improve code reusability and maintainability.
    
    Adds Application.UnitTests project to solution for upcoming test coverage.

---
+++ tests/UnitTests/Application.UnitTests/Codals/Common/FinancialStatementTitleGeneratorTest.cs
@@ -0,0 +1,56 @@
+﻿using Fundamental.Application.Codals.Common;
+using JetBrains.Annotations;
+using FluentAssertions;
+
+namespace Application.UnitTests.Codals.Common;
+
+[TestSubject(typeof(FinancialStatementTitleGenerator))]
+public class FinancialStatementTitleGeneratorTest
+{
+    [Theory]
+    [InlineData(
+        "گزارش صورت وضعیت مالی نماد ",
+        "TEST",
+        6,
+        12,
+        1402,
+        true,
+        "گزارش صورت وضعیت مالی نماد TEST ماه 6 دوره 6 ماهه  منتهی به 1402/12/29 حسابرسی شده")]
+    [InlineData(
+        "گزارش صورت سود و زیان نماد ",
+        "XYZ",
+        3,
+        12,
+        1402,
+        false,
+        "گزارش صورت سود و زیان نماد XYZ ماه 3 دوره 3 ماهه  منتهی به 1402/12/29 حسابرسی نشده")]
+    [InlineData(
+        "گزارش صورت وضعیت مالی نماد ",
+        "ABC",
+        12,
+        9,
+        1402,
+        true,
+        "گزارش صورت وضعیت مالی نماد ABC ماه 12 دوره 3 ماهه  منتهی به 1402/9/30 حسابرسی شده")]
+    public void GenerateTitle_ShouldGenerateCorrectTitle(
+        string reportType,
+        string symbol,
+        ushort reportMonth,
+        ushort yearEndMonth,
+        ushort fiscalYear,
+        bool isAudited,
+        string expected)
+    {
+        // Act
+        string result = FinancialStatementTitleGenerator.GenerateTitle(
+            reportType,
+            symbol,
+            reportMonth,
+            yearEndMonth,
+            fiscalYear,
+            isAudited);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
