33230c50411be7fb3bd413c4d00e9e5d7a8cd64a
Mostafa <mostafa.esmaeili8@gmail.com>
2025-10-28 19:01:16 +0330

    Fix MonthlyActivity table name: change from kebab-case 'monthly-activity' to snake_case 'monthly_activity'

---
+++ src/Infrastructure/Migrations/Fundamental/20251028153009_FixMonthlyActivityNaming.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Fundamental.Domain.Codals.Manufacturing.Enums;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fundamental.Migrations.Fundamental
+{
+    /// <inheritdoc />
+    public partial class FixMonthlyActivityNaming : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "monthly-activity",
+                schema: "manufacturing",
+                newName: "monthly_activity",
+                newSchema: "manufacturing");
+
+            migrationBuilder.AlterColumn<List<NoneOperationalIncomeTag>>(
+                name: "tags",
+                schema: "manufacturing",
+                table: "non-operation-income-expense",
+                type: "none_operational_income_tag[]",
+                nullable: false,
+                defaultValue: new List<NoneOperationalIncomeTag>(),
+                oldClrType: typeof(List<NoneOperationalIncomeTag>),
+                oldType: "none_operational_income_tag[]",
+                oldDefaultValue: new List<NoneOperationalIncomeTag>());
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "monthly_activity",
+                schema: "manufacturing",
+                newName: "monthly-activity",
+                newSchema: "manufacturing");
+
+            migrationBuilder.AlterColumn<List<NoneOperationalIncomeTag>>(
+                name: "tags",
+                schema: "manufacturing",
+                table: "non-operation-income-expense",
+                type: "none_operational_income_tag[]",
+                nullable: false,
+                defaultValue: new List<NoneOperationalIncomeTag>(),
+                oldClrType: typeof(List<NoneOperationalIncomeTag>),
+                oldType: "none_operational_income_tag[]",
+                oldDefaultValue: new List<NoneOperationalIncomeTag>());
+        }
+    }
+}
