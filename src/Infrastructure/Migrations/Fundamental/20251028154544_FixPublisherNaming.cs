94c8a05bba15724413516666c3ada433085c7297
Mostafa <mostafa.esmaeili8@gmail.com>
2025-10-28 19:17:06 +0330

    Fix Publisher foreign keys: change from kebab-case 'symbol-id' and 'parent-symbol-id' to snake_case 'symbol_id' and 'parent_symbol_id'

---
+++ src/Infrastructure/Migrations/Fundamental/20251028154544_FixPublisherNaming.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Fundamental.Domain.Codals.Manufacturing.Enums;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fundamental.Migrations.Fundamental
+{
+    /// <inheritdoc />
+    public partial class FixPublisherNaming : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<List<NoneOperationalIncomeTag>>(
+                name: "tags",
+                schema: "manufacturing",
+                table: "non_operation_income_and_expense",
+                type: "none_operational_income_tag[]",
+                nullable: false,
+                defaultValue: new List<NoneOperationalIncomeTag>(),
+                oldClrType: typeof(List<NoneOperationalIncomeTag>),
+                oldType: "none_operational_income_tag[]",
+                oldDefaultValue: new List<NoneOperationalIncomeTag>());
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<List<NoneOperationalIncomeTag>>(
+                name: "tags",
+                schema: "manufacturing",
+                table: "non_operation_income_and_expense",
+                type: "none_operational_income_tag[]",
+                nullable: false,
+                defaultValue: new List<NoneOperationalIncomeTag>(),
+                oldClrType: typeof(List<NoneOperationalIncomeTag>),
+                oldType: "none_operational_income_tag[]",
+                oldDefaultValue: new List<NoneOperationalIncomeTag>());
+        }
+    }
+}
